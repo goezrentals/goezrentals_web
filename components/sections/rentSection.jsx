1a736b08cbd65ccdc86448f5564e0bf58bae632c
Linkjavier <linkjavier@hotmail.com>
2023-08-17 19:43:46 -0500

    Terminado todo el modelo de seccion con el paralelogramo. COn el titulo centrado en mobile y corrido a la izquierda en escritorio. El paralelograma tambien se comporta diferente en mobile y en Desktop. AL igual que el fondo blanco con gradiente. En mobile es vertical el gradiente y en desktop es horizontal.

--- components/sections/rentSection.jsx
+++ components/sections/rentSection.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from 'next/router';
 import { PrismicNextLink } from '@prismicio/next';
+import { PrismicRichText } from "@prismicio/react"
+
 
 const RentSection = props => {
 
@@ -9,13 +11,29 @@ const RentSection = props => {
 
   return (
     <div className="bg-cover bg-center" style={{ backgroundImage: `url(${backgroundUrl}`}}>
-      <div className="flex flex-col w-full h-screen text-white justify-center">
-          <div className="text-4xl text-center font-bold">
+      <div className="flex flex-col w-full h-screen text-white justify-center centrar-webkit">
+
+          <div className="pt-2 pb-2 xl:-translate-x-60 translate-y-4 z-10 text-2xl text-center font-bold bg-[#e56608ff] -skew-x-12 xl:w-1/6 w-1/3 h-auto self-center">
               {titleText}
           </div>
-          {/* <div className="text-6xl bg-[#e56608ff] text-center w-min self-center paralelograma2 font-extrabold">
-              {bodyText}
-          </div> */}
+          <div className="z-1 w-full">
+            <div className="xl:text-2xl text-xl text-center self-center xl:w-1/2 w-4/5 text-black">
+              {/* <div className="z-10 text-black pr-20 pl-20 text-left translate-y-20">
+                  <PrismicRichText field={bodyText}/>
+              </div> */}
+              <div className="xl:rotate-6">
+                <div className="text-left pr-20 pl-20 pt-20 pb-20 xl:text-2xl text-xl text-center self-center xl:w-auto w-auto text-black -skew-y-6 background-paralelograma">
+                  <div className="text-left xl:-skew-y-0 skew-y-6">
+                    {/* I used this PrismicRichTect para saber el espacio que ocupa */}
+                    <PrismicRichText field={bodyText}/>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
       </div>
     </div>
   );
